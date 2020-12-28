31c5f28dc3994362b44df2ec18b956df347c808c
Folkas <73559010+Folkas@users.noreply.github.com>
2020-12-28 14:54:11 +0200

    indentations changed

--- functions/calculator.py
+++ functions/calculator.py
@@ -41,38 +41,39 @@ class calculator:
         """Returns the current value of calculator"""
         return self.__number
 
-#resets current number to zero    
+#resets current number to zero
     def reset(self) -> float:
         """Resets calculator's memory to 0"""
-      self.__number = 0
-      return self.__number
+        self.__number = 0
+        return self.__number
 
 #adds a number
     def add(self, input: float) -> float:
         """Takes in a float input, returns the sum of input and number"""
-      self.__number += input
-      return self.__number
+        self.__number += input
+        return self.__number
 
 #subtracts a number
     def subtract(self, input: float) -> float:
         """Takes in a float input, returns the difference of number and input"""
-      self.__number -= input
-      return self.__number
+        self.__number -= input
+        return self.__number
 
 #multiplies a number
     def multiply(self, input: float) -> float:
         """Takes in a float input, returns the product of number and input"""
-      self.__number *= input
-      return self.__number
+        self.__number *= input
+        return self.__number
 
 #divides a number
     def divide(self, input: float) -> float:
         """Takes in a float input, returns the quotient of number and input"""
-      self.__number /= input
-      return self.__number
+        self.__number /= input
+        return self.__number
 
 #takes nth root from a number
     def nroot(self, n: float) -> float:
         """Takes in a float n, returns the nth root of number"""
-      self.__number **= 1.00/n
-      return self.__number
+        self.__number **= 1.00/n
+        return self.__number
+
